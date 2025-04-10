19e78e410ebeaa8017df153a2532233d68dd7ce7
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-09 22:16:29 -0300

    fix: update all dependencies on project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,113 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LogInIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+// import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { Button } from "./_components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "./_components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./_components/ui/form";
+import { Input } from "./_components/ui/input";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
 const Home = () => {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  });
   return (
-    <div>
-      <h1>Ola blog Post</h1>
+    <div className="container space-y-5 py-5">
+      <Card>
+        <CardContent className="flex items-center justify-between p-5">
+          <h1>Home</h1>
+          <div>
+            <Button className="gap-x-2">
+              <LogInIcon />
+              Sign in
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Create user</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(() => {})} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-mail" type="email" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Password (min 8 characters)"
+                        type="password"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
