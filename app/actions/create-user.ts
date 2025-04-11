1f5dfde6797c4f9aaf50f7afc59218b56dd4bc3a
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-11 11:42:49 -0300

    feat: add action to create a new user

---
+++ app/actions/create-user.ts
@@ -0,0 +1,18 @@
+"use server";
+import bcrypjs from "bcryptjs";
+
+import { prisma } from "@/lib/prisma";
+
+import { FormSchema } from "../page";
+export const createUser = async (data: FormSchema) => {
+  //criptografar senha
+  const hashedPassword = await bcrypjs.hash(data.password, 8);
+  const user = await prisma.user.create({
+    data: {
+      name: data.name,
+      email: data.email,
+      password: hashedPassword,
+    },
+  });
+  return user;
+};
