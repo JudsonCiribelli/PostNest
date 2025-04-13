d9322f4db89a2e26fbfdf42c6b86637a804d4556
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2025-04-13 11:20:28 -0300

    feat: add app context to project

---
+++ app/Context/appContext.tsx
@@ -0,0 +1,15 @@
+import { Session } from "next-auth"; // Tipo da sessão do NextAuth
+import { createContext } from "react";
+
+interface AppContextType {
+  data: Session | null;
+  isPending: boolean;
+  startTransition: React.TransitionStartFunction;
+}
+
+// Criar o contexto já com valor inicial
+export const AppContext = createContext<AppContextType>({
+  data: null,
+  isPending: false,
+  startTransition: () => {},
+});
